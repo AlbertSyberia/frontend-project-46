a03c6184e5ef21f83b173c3bf9d45a23e04eb8fb
dramatist <albertsyberiia@yandex.ru>
2026-07-30 02:10:07 +0300

    modified step one

---
+++ __tests__/gendiff.test.js
@@ -0,0 +1,4 @@
+import genDiff from '@hexlet/code'
+
+const diff = genDiff('file1.json', 'file2.json')
+console.log(diff)
--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,6 +5,6 @@ const program = new Command()
 
 program
   .name('gendiff')
+  .version('1.0.0')
   .description('Compares two configuration files and shows a difference.')
-  .option('-V, --version', 'output the version number')
 program.parse()
