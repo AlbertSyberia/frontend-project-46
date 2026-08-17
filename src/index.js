830badd64009d692e46c19007e228384ba6b66ad
Dramdtist <lonewhales@gmail.com>
2026-08-17 23:55:53 +0300

    step five

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
-import _ from 'lodash'
+// import _ from 'lodash'
 
 export default function (file1, file2) {
-  _.isEqual(file1, file2)
+  const fileKeys1 = Object.keys(file1)
+  const fileKeys2 = Object.keys(file2)
+  const allKeys = Array.from(new Set([...fileKeys1, ...fileKeys2])).sort()
+
+  return allKeys.flatMap((key) => {
+    if (key in file1 && key in file2) {
+      if (file1[key] === file2[key]) {
+        return { key: key, value: file1[key] }
+      }
+      else {
+        const value1 = { key: key, value: file1[key] }
+        const value2 = { key: key, value: file2[key] }
+        return { value1, value2 }
+      }
+    }
+  })
 }
